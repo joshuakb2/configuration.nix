e1af641539855293f0ac4986dc0569eaefc66ec9
Josh Baker <jbaker@enseo.com>
2024-09-23 08:31:10 -0600

    Fixes for JBaker-LT

--- JBaker-LT/my-hardware-configs.nix
+++ JBaker-LT/my-hardware-configs.nix
@@ -1,16 +1,60 @@
 { pkgs, config, lib, ... }:
 {
   networking.hostName = "JBaker-LT";
-  nvidiaTweaks = false;
+  nvidiaTweaks = true;
   useGrub = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   myUserName = "jbaker";
   users.users."${config.myUserName}" = {
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "adbusers" "dialout"];
+    extraGroups = ["wheel" "networkmanager" "adbusers" "dialout" "docker"];
     description = "Josh Baker";
   };
 
+  virtualisation.docker.enable = true;
+
   boot.supportedFilesystems = ["ntfs"];
+
+  # DHCP for 10.250.11.0/24 network
+  services.kea.dhcp4 =
+    let
+      option = name: data: { inherit name data; };
+    in
+    {
+      enable = true;
+      settings = {
+        valid-lifetime = 21600;
+        interfaces-config = {
+          interfaces = ["enp0s31f6" "enp58s0u1"];
+        };
+        subnet4 = [
+          {
+            id = 1;
+            pools = [{ pool = "10.250.11.10 - 10.250.11.254"; }];
+            subnet = "10.250.11.0/24";
+            option-data = [
+              (option "domain-name" "stbs.local")
+              (option "domain-name-servers" "8.8.8.8")
+              (option "routers" "10.250.11.1")
+            ];
+          }
+          {
+            id = 2;
+            pools = [{ pool = "172.18.0.2 - 172.18.0.32"; }];
+            subnet = "172.18.0.0/24";
+            option-data = [
+              (option "domain-name" "hotel.local")
+              (option "domain-name-servers" "8.8.8.8")
+              (option "routers" "172.18.0.1")
+            ];
+          }
+        ];
+        lease-database = {
+          type = "memfile";
+          persist = true;
+          name = "/var/lib/kea/kea-leases4.csv";
+        };
+      };
+    };
 }
