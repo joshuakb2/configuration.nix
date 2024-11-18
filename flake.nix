6c70b408ec8f49b9c5186b19ed9e4c243b7722f6
Joshua Baker <joshuakb2@gmail.com>
2024-11-17 19:42:31 -0700

    Remove hyprland flake inputs

--- flake.nix
+++ flake.nix
@@ -12,28 +12,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Hyprland stuff
-    aquamarine = {
-      type = "git";
-      url = "https://github.com/hyprwm/aquamarine";
-      ref = "refs/tags/v0.4.1";
-      inputs.nixpkgs.follows = "nixpkgs-2024-july";
-    };
-
-    hyprland = {
-      type = "git";
-      url = "https://github.com/hyprwm/hyprland";
-      ref = "refs/tags/v0.43.0";
-      submodules = true;
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.aquamarine.follows = "aquamarine";
-    };
-
-    hyprgrass = {
-      url = "github:horriblename/hyprgrass";
-      inputs.hyprland.follows = "hyprland";
-    };
+    hyprgrass.url = "github:horriblename/hyprgrass";
   };
 
   outputs = { self, nixpkgs, ...}@inputs:
@@ -43,7 +22,6 @@
           inputs.neovim-nightly-overlay.overlays.default
           inputs.openconnect-overlay.overlay
           nixpkgs-2024-july-overlay
-          hyprlandOverlay
         ];
       };
       other-nixpkgs-args = system: {
@@ -66,29 +44,6 @@
           config.allowUnfree = true;
         };
       };
-      hyprlandOverlay = final: prev: {
-        hyprland = let
-          libinput = prev.libinput.overrideAttrs (self: {
-            name = "libinput";
-            version = "1.26.0";
-            src = final.fetchFromGitLab {
-              domain = "gitlab.freedesktop.org";
-              owner = "libinput";
-              repo = "libinput";
-              rev = self.version;
-              hash = "sha256-mlxw4OUjaAdgRLFfPKMZDMOWosW9yKAkzDccwuLGCwQ=";
-            };
-          });
-        in
-          inputs.hyprland.packages.${prev.system}.hyprland.override {
-            libinput = libinput;
-            aquamarine = inputs.hyprland.inputs.aquamarine.packages.${prev.system}.aquamarine.override {
-              libinput = libinput;
-            };
-            # need this to fix meson build for some reason?
-            wayland-scanner = final.nixpkgs-2024-july.wayland-scanner;
-          };
-       };
     in {
       nixosConfigurations.Joshua-PC-Nix = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
