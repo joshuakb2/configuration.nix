6dbbe48b213017971e592fed6ba40dc585c85bf6
Joshua Baker <joshuakb2@gmail.com>
2024-12-02 03:46:42 -0700

    More hyprland fixes

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -8,5 +8,6 @@
   };
 
   xdg.configFile."hypr/hyprland.common.conf".source = ./hyprland.common.conf;
+  xdg.configFile."hypr/hyprland.host.conf".text = ""; # Just ensure file exists, it can be empty.
   xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
 }
