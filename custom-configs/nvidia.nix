f1c6dba9c2762f342fd17e702968a03b8c4c018a
Joshua Baker <joshuakb2@gmail.com>
2025-01-23 16:48:39 -0700

    Improve nvidia config

--- custom-configs/nvidia.nix
+++ custom-configs/nvidia.nix
@@ -9,32 +9,27 @@
     };
   };
 
-  config = {
+  config = lib.mkIf config.nvidiaTweaks {
     # Needed for suspend to work correctly, I'm told by Hyprland
-    boot.kernelParams = lib.mkIf config.nvidiaTweaks [
+    boot.kernelParams = [
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-      "nvidia_drm.fbdev=1" # Without this, TTY framebuffers don't work (Ctrl-Alt-Fn)
     ];
 
-    hardware.graphics = lib.mkIf config.nvidiaTweaks {
+    hardware.graphics = {
       enable = true;
-      extraPackages = [pkgs.libvdpau-va-gl];
+      # Disabled because I'm not sure if this is still needed or not? Does Chromium video decoding work without this?
+      # extraPackages = [pkgs.libvdpau-va-gl]; # Necessary for Chromium hardware accelerated video decoding
       enable32Bit = true;
     };
 
-    services.xserver.videoDrivers = lib.mkIf config.nvidiaTweaks ["nvidia"];
+    services.xserver.videoDrivers = ["nvidia"];
 
-    hardware.nvidia = lib.mkIf config.nvidiaTweaks {
+    hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.latest;
       modesetting.enable = true;
       powerManagement.enable = false;
       powerManagement.finegrained = false;
       open = false;
-    };
-
-    environment.variables = lib.mkIf config.nvidiaTweaks {
-      VDPAU_DRIVER = "va_gl";
-      LIBVA_DRIVER_NAME = "nvidia";
     };
   };
 }
