e3dcb9bb55d4b0949a0051b2cc3a1533d8bb5fa2
Josh Baker <jbaker@enseo.com>
2025-06-29 12:23:14 -0600

    JBaker-LT: Stop logging DHCP server to file

--- JBaker-LT/my-hardware-configs.nix
+++ JBaker-LT/my-hardware-configs.nix
@@ -75,19 +75,6 @@
     {
       enable = true;
       settings = {
-        loggers = [
-          {
-            name = "kea-dhcp4";
-            severity = "DEBUG";
-            debuglevel = 99;
-            output_options = [
-              {
-                output = "/var/log/kea.log";
-                flush = true;
-              }
-            ];
-          }
-        ];
         authoritative = true;
         min-valid-lifetime = 10800;
         valid-lifetime = 21600;
