7f66cc8481f861e6ec09f65f9e57953e45fbaa39
Joshua Baker <joshuakb2@gmail.com>
2025-07-09 14:57:51 -0600

    Rekey secrets with multiple keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,14 @@
 let
-  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYEtwhOUhooRNQ2KX/tQOyjQ+H3xRQcl87B2gGk3yp2";
+  Joshua-PC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYEtwhOUhooRNQ2KX/tQOyjQ+H3xRQcl87B2gGk3yp2";
+  JBaker-LT = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFroq4+XRzI2mZtA4FQjV41vjx2eLln/C3n0NLMloTHH";
+  joshuabaker-me = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhWVW7ixMAKKsO9V/JLyt1FGkNtkAlLa1ttLpk6BmIL";
 
+  publicKeys = [
+    Joshua-PC
+    JBaker-LT
+    joshuabaker-me
+  ];
 in
 {
-  "ddns-updater-config-Joshua-PC.age".publicKeys = [ system1 ];
+  "ddns-updater-config-Joshua-PC.age".publicKeys = publicKeys;
 }
