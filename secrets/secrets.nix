4c67f8108da518139cd58b7de64aeccfb08e0fd4
Joshua Baker <joshuakb2@gmail.com>
2025-08-09 12:21:18 -0600

    Add Joshua-X1 private key and rekey all secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,11 +2,13 @@ let
   Joshua-PC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYEtwhOUhooRNQ2KX/tQOyjQ+H3xRQcl87B2gGk3yp2";
   JBaker-LT = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFroq4+XRzI2mZtA4FQjV41vjx2eLln/C3n0NLMloTHH";
   joshuabaker-me = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhWVW7ixMAKKsO9V/JLyt1FGkNtkAlLa1ttLpk6BmIL";
+  Joshua-X1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoxcnKNlQTtFnf3jhrxEXGPZhvjwTlvZKgCjvNPOov3 root@Joshua-X1";
 
   publicKeys = [
     Joshua-PC
     JBaker-LT
     joshuabaker-me
+    Joshua-X1
   ];
 in
 {
